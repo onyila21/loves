50df3c23c7d583b1210af27d3b6c1d64f9c8df2f
onyila21 <onyinyechi830@gmail.com>
2022-12-06 09:41:21 +0100

    initial commit

---
+++ src/love.js
@@ -0,0 +1,48 @@
+const love= [
+  {
+    id: 1,
+    emoji: "💞",
+    name: "harp",
+    meaning:
+      "“Traditionally, the harp symbolizes love in Celtic culture, acting as a link between heaven and earth. It represents a love bridge between heaven and earth. The soothing tones of a harp have been traditionally employed in love ballads. According to tradition, King David played the harp for the Lord to demonstrate His endless dedication and love for the Lord.."
+  },
+  {
+    id: 2,
+    emoji: "💝",
+    name: "swans",
+    meaning:
+      "swans symbolize Venus, the Roman goddess of love, and Aphrodite, the Greek goddess of love and passion. Swans with connected beaks, sitting opposite one another, are the symbol of love most prevalent in the art world. Their necks are believed to be shaped like a heart."
+
+
+  },
+  {
+    id: 3,
+    emoji: "✨",
+    name: "Rose Quartz",
+    meaning:
+      "Rose quartz has been referenced in mythology and folklore all across the globe, from Greece to Egypt to China. This pink stone has been considered a sign of love since 600BC. It’s believed that combining rose quartz with meditation may help develop self-love and attract romantic love“."
+  },
+  {
+    id: 4,
+    emoji: "💒",
+    name: "The Claddagh ring",
+    meaning:
+      "Inspireded by Irish mythology, the Claddagh ring symbolizes love with a crown, two hands, and a heart. The Claddagh ring is a traditional piece. An Irish wedding, engagement, and friendship band are all common uses for this ring in Ireland. It shows two hands clasping a crown-topped heart.."
+  },
+  {
+    id: 5,
+    emoji: "♥️",
+    name: "Apple",
+    meaning:
+      "Apple appears as a symbol of love in numerous tales and mythologies. From Adam and Eve and the forbidden apple in the Bible to its current status as the “fruit of wisdom,” it has drawn much attention throughout history. Venus, the Roman goddess of love and passion, is often shown in ancient art holding an apple. In Norse, Greek, and Chinese mythology, the Gods ate golden apples to increase their youth and vigor.."
+  },
+  {
+    id: 6,
+    emoji: "💕",
+    name: "Infinity",
+    meaning:
+      "Another sign of love is the infinity symbol. The infinity symbol is similar to the Celtic love knot in that it has no beginning or end. Infinity has been used as a love metaphor throughout history, whether in ancient Greece, Rome, India, or Tibet."
+  }
+];
+
+export default love;
